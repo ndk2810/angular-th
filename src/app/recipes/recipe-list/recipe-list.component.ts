5487f6bf28cb9c1c95b5aff546b89f78a0c66999
ndk2810 <khangdeptrai2810@gmail.com>
2022-03-25 11:06:12 +0700

    t6 25/3

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,8 @@ import { Component, OnInit } from '@angular/core';
 
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://assets.bonappetit.com/photos/5c2f8fe26558e92c8a622671/1:1/w_2700,h_2700,c_limit/bolognese-1.jpg')
+    new Recipe('Pizza', 'This is simply a test', 'https://assets.bonappetit.com/photos/5c2f8fe26558e92c8a622671/1:1/w_2700,h_2700,c_limit/bolognese-1.jpg'),
+    new Recipe('Spaghett', 'This is simply a test', 'https://cdn.daynauan.info.vn/wp-content/uploads/2018/03/mi-y-chay.jpg')
   ];
   constructor() { }
   ngOnInit(): void {
