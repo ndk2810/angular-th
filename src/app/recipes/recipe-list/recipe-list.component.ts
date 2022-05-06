db183c26c5cd0eba1b774ace1811e38ad2d2bf5e
ndk2810 <khangdeptrai2810@gmail.com>
2022-05-06 09:28:09 +0700

    cham hoi

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,8 +9,8 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://assets.bonappetit.com/photos/5c2f8fe26558e92c8a622671/1:1/w_2700,h_2700,c_limit/bolognese-1.jpg'),
-    new Recipe('A Test Recipe 2', 'This is simply a test 2', 'https://assets.bonappetit.com/photos/5c2f8fe26558e92c8a622671/1:1/w_2700,h_2700,c_limit/bolognese-1.jpg'),
+    new Recipe('Pizza', 'Delicious pepperoni pizza', 'https://assets.bonappetit.com/photos/5c2f8fe26558e92c8a622671/1:1/w_2700,h_2700,c_limit/bolognese-1.jpg'),
+    new Recipe('Lasagna', 'Pure italian bliss', 'https://www.hoteljob.vn/files/Anh-HTJ-Hong/lasagna-la-gi-2.jpg'),
 ];
 
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
